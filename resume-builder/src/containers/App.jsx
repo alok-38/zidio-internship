76a85e13e4a3a399620fbb96f31c331494aac45f
Alokananda Y <plantsfortrees@gmail.com>
2024-06-04 08:11:58 +0530

    routing with lazy loading

--- resume-builder/src/containers/App.jsx
+++ resume-builder/src/containers/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { HomeScreen, Authentication } from "../pages";
 
 const App = () => {
   return (
-	<div className='text-5xl'>App</div>
-  )
-}
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/*" element={<HomeScreen />} />
+        <Route path="/auth" element={<Authentication />} />
+      </Routes>
+    </Suspense>
+  );
+};
 
-export default App
+export default App;
