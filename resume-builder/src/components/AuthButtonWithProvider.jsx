43c3834ac9fc90b38db52c715463c433a087477f
Alokananda Y <plantsfortrees@gmail.com>
2024-06-04 10:37:05 +0530

    AuthButtonWithProvider

--- resume-builder/src/components/AuthButtonWithProvider.jsx
+++ resume-builder/src/components/AuthButtonWithProvider.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { FaChevronRight } from "react-icons/fa6";
 
 const AuthButtonWithProvider = ({ Icon, label, provider }) => {
   return (
     <div
-      className="w-full px-4 py-3 rounded-md border-2 border-blue-700 flex items-center justify-center cursor-pointer group hover:bg-blue-700
-    active:scale-95 duration-150 hover:shadow-md hover:text-white"
+      className="w-full px-4 py-3 rounded-md border-2 border-blue-700 flex items-center justify-between cursor-pointer group hover:bg-blue-700
+    active:scale-95 duration-150 hover:shadow-md"
     >
-      AuthButtonWithProvider
+      <Icon className="text-txtPrimary text-xl group-hover:text-white" />
+      <p className="text-txtPrimary text-lg group-hover:text-white">{label}</p>
+      <FaChevronRight className="text-txtPrimary text-base group-hover:text-white" />
     </div>
   );
 };
