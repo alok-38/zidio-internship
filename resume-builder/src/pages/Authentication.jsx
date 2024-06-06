be74921eb18daf2a3c2839c2b35e22ad68677e93
Alokananda Y <plantsfortrees@gmail.com>
2024-06-06 19:43:58 +0530

    Added Google and Github buttons

--- resume-builder/src/pages/Authentication.jsx
+++ resume-builder/src/pages/Authentication.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Logo } from "../assets";
+import { AuthButtonWithProvider } from "../components";
 import { Footer } from "../containers";
+import { FaGoogle, FaGithub } from "react-icons/fa6";
 
 const Authentication = () => {
   return (
@@ -8,6 +10,28 @@ const Authentication = () => {
       {/* top section */}
       <img src={Logo} alt="Logo" className="w-12 h-auto object-contain" />
       {/* Main */}
+      <div className="mt-40  w-full flex flex-1 flex-col items-center justify-center gap-6">
+        <h1 className="text-3xl lg:text-4xl text-blue-700">
+          Welcome to Expressume
+        </h1>
+        <p className="text-base text-txtPrimary">
+          express way to create resume
+        </p>
+
+        <div className="w-full lg:w-96 p-4 rounded-md flex flex-col items-center justify-start gap-6">
+          <AuthButtonWithProvider
+            Icon={FaGoogle}
+            label={"Signin with Google"}
+            provider={"GoogleAuthProvider"}
+          />
+
+          <AuthButtonWithProvider
+            Icon={FaGithub}
+            label={"Signin with GitHub"}
+            provider={"GitHubAuthProvider"}
+          />
+        </div>
+      </div>
       <div className="w-full flex flex-1 flex-col items-center gap-6 justify-center"></div>
       {/* Footer */}
       <Footer />
