e6179615f97991d579ba6ce0195ecb0c8ac30f2a
Alokananda Y <plantsfortrees@gmail.com>
2024-06-07 09:02:44 +0530

    Query client

--- resume-builder/src/containers/App.jsx
+++ resume-builder/src/containers/App.jsx
@@ -1,17 +1,24 @@
 import React, { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 const HomeScreen = lazy(() => import("../pages/HomeScreen"));
 const Authentication = lazy(() => import("../pages/Authentication"));
 
 const App = () => {
+  const queryClient = new QueryClient();
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/*" element={<HomeScreen />} />
-        <Route path="/auth" element={<Authentication />} />
-      </Routes>
-    </Suspense>
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/*" element={<HomeScreen />} />
+          <Route path="/auth" element={<Authentication />} />
+        </Routes>
+      </Suspense>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
