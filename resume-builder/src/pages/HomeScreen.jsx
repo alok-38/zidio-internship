b281ca7a8f0b1ee676df97ea844a10a15813fa20
Alokananda Y <plantsfortrees@gmail.com>
2024-06-07 18:44:25 +0530

    home screen routing is working

--- resume-builder/src/pages/HomeScreen.jsx
+++ resume-builder/src/pages/HomeScreen.jsx
@@ -3,8 +3,13 @@ import { Header, MainSpinner } from "../components";
 import { Routes, Route } from "react-router-dom";
 import { HomeContainer } from "../containers";
 
-// Lazy load the CreateTemplate page
+// Lazy load the pages
+const UserProfile = lazy(() => import("../pages/UserProfile"));
 const CreateTemplate = lazy(() => import("../pages/CreateTemplate"));
+const CreateResume = lazy(() => import("../pages/CreateResume"));
+const TemplateDesignPinDetails = lazy(() =>
+  import("../pages/TemplateDesignPinDetails")
+);
 
 const HomeScreen = () => {
   return (
@@ -15,6 +20,12 @@ const HomeScreen = () => {
           <Routes>
             <Route path="/" element={<HomeContainer />} />
             <Route path="/template/create" element={<CreateTemplate />} />
+            <Route path="/profile/:uid" element={<UserProfile />} />
+            <Route path="/resume/*" element={<CreateResume />} />
+            <Route
+              path="/resumeDetail/:templateID"
+              element={<TemplateDesignPinDetails />}
+            />
           </Routes>
         </Suspense>
       </main>
