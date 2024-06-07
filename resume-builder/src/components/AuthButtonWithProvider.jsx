7b928ede79facee5c28abdc8c0a835ebe4c763d9
Alokananda Y <plantsfortrees@gmail.com>
2024-06-07 05:59:01 +0530

    AuthButtonWithProvider

--- resume-builder/src/components/AuthButtonWithProvider.jsx
+++ resume-builder/src/components/AuthButtonWithProvider.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { FaChevronRight } from "react-icons/fa6";
 
-const AuthButtonWithProvider = ({ Icon, label }) => {
+const AuthButtonWithProvider = ({ Icon, label, provider }) => {
+  const handleClick = async () => {
+    if (provider === "GoogleAuthProvider") {
+      console.log("Inside Google auth");
+    } else if (provider === "GitHubAuthProvider") {
+      console.log("Inside Github auth");
+    } else {
+      console.log("Inside Google auth");
+    }
+  };
   return (
-    <div className="w-full px-4 py-3 rounded-md border-2 border-blue-700 flex items-center justify-between cursor-pointer group hover:bg-blue-700 active:scale-95 duration-150 hover:shadow-md">
+    <div
+      onClick={handleClick}
+      className="w-full px-4 py-3 rounded-md border-2 border-blue-700 flex items-center justify-between cursor-pointer group hover:bg-blue-700 active:scale-95 duration-150 hover:shadow-md"
+    >
       <Icon className="text-txtPrimary text-xl group-hover:text-white" />
       <p className="text-txtPrimary text-lg group-hover:text-white">{label}</p>
       <FaChevronRight className="text-txtPrimary text-base group-hover:text-white" />
