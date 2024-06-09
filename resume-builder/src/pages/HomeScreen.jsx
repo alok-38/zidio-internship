f8eb1376d3b684d0926e9117defb896bba233085
Alokananda Y <plantsfortrees@gmail.com>
2024-06-09 15:46:06 +0530

    Create new template

--- resume-builder/src/pages/HomeScreen.jsx
+++ resume-builder/src/pages/HomeScreen.jsx
@@ -3,6 +3,8 @@ import { Header, MainSpinner } from "../components";
 import { Route, Routes } from "react-router-dom";
 import HomeContainer from "../containers/HomeContainer"; // Ensure correct import path
 import CreateTemplate from "./CreateTemplate";
+import CreateResume from "./CreateResume";
+import  CreateProfile  from "./CreateProfile";
 
 const HomeScreen = () => {
   return (
@@ -13,6 +15,8 @@ const HomeScreen = () => {
           <Routes>
             <Route path="/" element={<HomeContainer />} />
             <Route path="/template/create" element={<CreateTemplate />} />
+            <Route path="/profile/:uid" element={<CreateProfile />} />
+            <Route path="/resume/*" element={<CreateResume />} />
           </Routes>
         </Suspense>
       </main>
