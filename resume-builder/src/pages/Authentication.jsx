2844bdf86a37fbf16bbef5cd96e31882ea73bb9d
Alokananda Y <plantsfortrees@gmail.com>
2024-06-09 10:23:11 +0530

    Applied the footer

--- resume-builder/src/pages/Authentication.jsx
+++ resume-builder/src/pages/Authentication.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Logo } from "../assets";
+import Footer from "../containers/Footer";
 
 const Authentication = () => {
   return (
-	<div className='auth-section'>Authentication</div>
-  )
-}
+    <div className="auth-section">
+      {/* top section */}
+      <img src={Logo} className="w-12 h-auto object-contain" alt="logo" />
+      {/* main section */}
+      <div className="w-full flex flex-1 flex-col items-center justify-center gap-6"></div>
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
 
-export default Authentication
+export default Authentication;
