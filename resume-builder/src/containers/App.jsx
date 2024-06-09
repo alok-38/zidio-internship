adf4a0c5a33d6a1ab60ed72aa6a1eb021e9e60d3
Alokananda Y <plantsfortrees@gmail.com>
2024-06-09 12:32:23 +0530

    React query client dev tools

--- resume-builder/src/containers/App.jsx
+++ resume-builder/src/containers/App.jsx
@@ -1,15 +1,22 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { HomeScreen, Authentication } from "../pages";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/*" element={<HomeScreen />} />
-        <Route path="/auth" element={<Authentication />} />
-      </Routes>
-    </Suspense>
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/*" element={<HomeScreen />} />
+          <Route path="/auth" element={<Authentication />} />
+        </Routes>
+      </Suspense>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
