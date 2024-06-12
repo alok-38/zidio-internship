5e8f2e5155c018905aef775f171a9c955f9906cf
Alokananda Y <plantsfortrees@gmail.com>
2024-06-12 08:16:45 +0530

    Designed the Google and GitHub buttons

--- resume-builder-cra/src/pages/Authentication.jsx
+++ resume-builder-cra/src/pages/Authentication.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Logo } from "../assets";
+import AuthButtonWithProvider from "../components/AuthButtonWithProvider";
 import Footer from "../containers/Footer";
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
 const Authentication = () => {
   return (
@@ -9,7 +11,22 @@ const Authentication = () => {
       <img src={Logo} alt="logo" className="w-12 h-auto object-contain" />
       {/* main */}
       <div className="w-full flex flex-1 flex-col items-center justify-center gap-6">
-
+        <h1 className="3xl lg:text-4xl text-orange-500">
+          Welcome To Express Resume
+        </h1>
+        <p className="text-2xl text-gray-600">Express way to create resume</p>
+        <div className="w-full lg:w-96 rounded-md p-2 flex flex-col items-center justify-start gap-6">
+          <AuthButtonWithProvider
+            Icon={FaGoogle}
+            label="Sign in with Google"
+            provider="GoogleAuthProvider"
+          />
+          <AuthButtonWithProvider
+            Icon={FaGithub}
+            label="Sign in with GitHub"
+            provider="GitHubAuthProvider"
+          />
+        </div>
       </div>
       {/* footer */}
       <Footer />
