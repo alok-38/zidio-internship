80f471e837e9a30e6413dd7ac939f6c42805eaa4
Alokananda Y <plantsfortrees@gmail.com>
2024-06-12 07:33:14 +0530

    Footer section

--- resume-builder-cra/src/pages/Authentication.jsx
+++ resume-builder-cra/src/pages/Authentication.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Logo } from "../assets";
+import Footer from "../containers/Footer";
 
 const Authentication = () => {
   return (
-	<div>Authentication</div>
-  )
-}
+    <div className="auth-section">
+      {/* top section */}
+      <img src={Logo} alt="logo" className="w-12 h-auto object-contain" />
+      {/* main */}
+      <div className="w-full flex flex-1 flex-col items-center justify-center gap-6">
 
-export default Authentication
+      </div>
+      {/* footer */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Authentication;
