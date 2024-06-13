5f9b76c99052dcd6314ff9fb38154301b2505949
Alokananda Y <plantsfortrees@gmail.com>
2024-06-13 13:49:49 +0530

    All routes are working

--- resume-builder-cra/src/pages/Home.jsx
+++ resume-builder-cra/src/pages/Home.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
+import MainSpinner from "../components/MainSpinner";
+import HomeContainer from "../containers/HomeContainer";
+import CreateTemplate from "../pages/CreateTemplate";
+import UserProfile from "../pages/UserProfile";
+import CreateResume from "../pages/CreateResume";
+import TemplateDesignPinDetails from "../pages/TemplateDesignPinDetails";
 
 const Home = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center">
       {/* header */}
       <Header />
-      <main>{/* main component */}</main>
+      <main className="w-full">
+        {/* main component */}
+        <Suspense fallback={<MainSpinner />}>
+          <Routes>
+            <Route path="/" element={<HomeContainer />} />
+            <Route path="/template/create" element={<CreateTemplate />} />
+            <Route path="/profile/:uid" element={<UserProfile />} />
+            <Route path="/resume/*" element={<CreateResume />} />
+            <Route
+              path="/resumeDetail/:templateID"
+              element={<TemplateDesignPinDetails />}
+            />
+          </Routes>
+        </Suspense>
+      </main>
     </div>
   );
 };
--- resume-builder-cra/src/pages/index.js
+++ resume-builder-cra/src/pages/index.js
@@ -1,3 +1,7 @@
 export { default as Home } from "./Home";
 
 export { default as Authentication } from "./Authentication";
+export { default as CreateTemplate } from "./CreateTemplate";
+export { default as UserProfile } from "./UserProfile";
+export { default as CreateResume } from "./CreateResume";
+export { default as TemplateDesignPinDetails } from "./TemplateDesignPinDetails";
