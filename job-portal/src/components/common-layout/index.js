f881461157688845baa1152e7902e9b3b5556308
Alokananda Y <plantsfortrees@gmail.com>
2024-06-14 08:05:09 +0530

    Common layout

---
+++ job-portal/src/components/common-layout/index.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+function CommonLayout({ children }) {
+  return (
+    <div className="mx-auto max-w-7xl p-6 lg:px-8">
+      {/* Header Component */}
+
+      {/* Header Component */}
+
+      {/* Main Content */}
+      <main>{children}</main>
+
+      {/* Main Content */}
+    </div>
+  );
+}
+
+export default CommonLayout;
