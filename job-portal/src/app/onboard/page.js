e68045b922f19c81f1b8cbc0bdd434d284d8aa0e
Alokananda Y <plantsfortrees@gmail.com>
2024-06-14 14:58:46 +0530

    mongodb

--- job-portal/src/app/onboard/page.js
+++ job-portal/src/app/onboard/page.js
@@ -1,5 +1,20 @@
-function OnBoard() {
-  return <div>This is our onboard page</div>;
+import { fetchProfileAction } from "@/actions";
+import OnBoard from "@/components/on-board";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+async function OnBoardPage() {
+  //get the auth user from clerk
+  const user = await currentUser();
+
+  //fetch the profile info -> either user is candidate / user is recruiter
+  const profileInfo = await fetchProfileAction(user?.id);
+
+  if (profileInfo?._id) {
+    if (profileInfo?.role === "recruiter" && !profileInfo.isPremiumUser)
+      redirect("/membership");
+    else redirect("/");
+  } else return <OnBoard />;
 }
 
-export default OnBoard;
+export default OnBoardPage;
--- job-portal/src/components/common-layout/index.js
+++ job-portal/src/components/common-layout/index.js
@@ -1,10 +1,13 @@
 import Header from "../header";
+import { currentUser } from '@clerk/nextjs/server';
 
-function CommonLayout({ children }) {
+async function CommonLayout({ children }) {
+
+  const user = await currentUser();
   return (
     <div className="mx-auto max-w-7xl p-6 lg:px-8">
       {/* Header Component */}
-      <Header />
+      <Header user={JSON.parse(JSON.stringify(user))}/>
       {/* Header Component */}
       {/* Main Content */}
       <main>{children}</main>
