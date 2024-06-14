4255606bcb81fd6d384dab421d52b16b6a523ec8
Alokananda Y <plantsfortrees@gmail.com>
2024-06-14 09:21:36 +0530

    Header and main content

---
+++ job-portal/src/components/common-layout/index.js
@@ -0,0 +1,14 @@
+function CommonLayout({ children }) {
+  return (
+    <div className="mx-auto max-w-7xl p-6 lg:px-8">
+      {/* Header Component */}
+      Header
+      {/* Header Component */}
+      {/* Main Content */}
+      <main>{children}</main>
+      {/* Main Content */}
+    </div>
+  );
+}
+
+export default CommonLayout;
