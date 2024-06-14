66aa0b0e66fb6a66169918f88526d21dec5c71fa
Alokananda Y <plantsfortrees@gmail.com>
2024-06-14 15:10:37 +0530

    Components

---
+++ job-portal/src/components/candidate-activity/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+import CommonCard from "../common-card";
+import JobIcon from "../job-icon";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+
+function CandidateActivity({ jobList, jobApplicants }) {
+  console.log(jobList, jobApplicants);
+
+  const uniqueStatusArray = [
+    ...new Set(
+      jobApplicants.map((jobApplicantItem) => jobApplicantItem.status).flat(1)
+    ),
+  ];
+
+  console.log(uniqueStatusArray);
+
+  return (
+    <div className="mx-auto max-w-7xl">
+      <Tabs defaultValue="Applied" className="w-full">
+        <div className="flex items-baseline dark:border-white justify-between border-b pb-6 pt-24">
+          <h1 className="text-4xl font-bold dark:text-white tracking-tight text-gray-950">
+            Your Activity
+          </h1>
+          <TabsList>
+            {uniqueStatusArray.map((status, index) => (
+              <TabsTrigger key={index} value={status}>
+                {status}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </div>
+        <div className="pb-24 pt-6">
+          <div className="container mx-auto p-0 space-y-8">
+            <div className="flex flex-col gap-4">
+              {uniqueStatusArray.map((status, index) => (
+                <TabsContent key={index} value={status}>
+                  {jobList
+                    .filter(
+                      (jobItem) =>
+                        jobApplicants
+                          .filter(
+                            (jobApplication) =>
+                              jobApplication.status.indexOf(status) > -1
+                          )
+                          .findIndex(
+                            (filteredItemByStatus) =>
+                              jobItem._id === filteredItemByStatus.jobID
+                          ) > -1
+                    )
+                    .map((finalFilteredItem, index) => (
+                      <CommonCard
+                        key={index}
+                        icon={<JobIcon />}
+                        title={finalFilteredItem?.title}
+                        description={finalFilteredItem?.companyName}
+                      />
+                    ))}
+                </TabsContent>
+              ))}
+            </div>
+          </div>
+        </div>
+      </Tabs>
+    </div>
+  );
+}
+
+export default CandidateActivity;
