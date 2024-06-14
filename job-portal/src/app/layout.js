2b459abeb8e9b2a189711c1cdf1ec29bf4414464
Alokananda Y <plantsfortrees@gmail.com>
2024-06-14 05:59:49 +0530

    Applying common layout

--- job-portal/src/app/layout.js
+++ job-portal/src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Loading from "./loading";
+import { Suspense } from "react";
+import CommonLayout from "@/components/common-layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Suspense fallback={<Loading />}>
+          <CommonLayout children={children} />
+        </Suspense>
+      </body>
     </html>
   );
 }
---
+++ job-portal/src/components/common-layout/index.js
@@ -0,0 +1,9 @@
+function CommonLayout({ children }) {
+  return <div className="mx-auto mx-w-7xl p-6 lg:px-8">
+	{/* header */}
+	{/* main */}
+	<main>
+		{children}
+	</main>
+  </div>;
+}
