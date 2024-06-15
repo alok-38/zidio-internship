bd06218bec6ca73856f81284bc7d8ec4f6b3f5a6
Alokananda Y <plantsfortrees@gmail.com>
2024-06-15 07:13:44 +0530

    Routes

--- resume-builder/src/containers/App.jsx
+++ resume-builder/src/containers/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { HomeScreen, Authentication } from "../pages";
 
 function App() {
   return (
-	<div className='text-5xl items-center flex justify-center'>App</div>
-  )
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/*" element={<HomeScreen />} />
+        <Route path="/auth" element={<Authentication />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
