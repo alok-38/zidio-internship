8c93fad5e17e15816f95120a6a518e866c5fa858
Alokananda Y <plantsfortrees@gmail.com>
2024-06-15 07:54:06 +0530

    Buttons

---
+++ resume-builder/src/components/AuthButtonWithProvider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthButtonWithProvider = ({Icon, label, provider}) => {
+  return (
+	<div>AuthButtonWithProvider</div>
+  )
+}
+
+export default AuthButtonWithProvider
--- resume-builder/src/pages/Authentication.jsx
+++ resume-builder/src/pages/Authentication.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Logo } from "../assets";
 import { Footer } from "../containers";
+import { AuthButtonWithProvider } from "../components";
 
 const Authentication = () => {
   return (
@@ -8,7 +9,17 @@ const Authentication = () => {
       {/* top section */}
       <img className="w-12 h-auto object-contain" src={Logo} alt="logo" />
       {/* main section */}
-      <div className="w-full flex flex-1 flex-col items-center justify-center gap-6"></div>
+      <div className="w-full flex flex-1 flex-col items-center justify-center gap-6">
+        <h1 className="text-orange-500 text-3xl lg:text-4xl">
+          Welcome to Zidio Resume
+        </h1>
+        <p className="text-xl text-gray-700 italic">
+          Innovative way to create resume
+        </p>
+        <div className="w-full lg:w-96 rounded-md p-2 flex flex-col items-center justify-start gap-6"></div>
+        <AuthButtonWithProvider />
+        <AuthButtonWithProvider />
+      </div>
       {/* footer section */}
       <Footer />
     </div>
