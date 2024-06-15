176c1e06f679d03a894ee128cffc8b3ed7cf3eb7
Alokananda Y <plantsfortrees@gmail.com>
2024-06-15 14:04:15 +0530

    Env

--- resume-builder/src/config/firebase.config.js
+++ resume-builder/src/config/firebase.config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBu1TYm9u1p0YhmcEEcZhEzBh6kn7qFtFs",
-  authDomain: "resume-builder-f6ba9.firebaseapp.com",
-  projectId: "resume-builder-f6ba9",
-  storageBucket: "resume-builder-f6ba9.appspot.com",
-  messagingSenderId: "869210922083",
-  appId: "1:869210922083:web:2df073c4334c3844f4ec56",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
