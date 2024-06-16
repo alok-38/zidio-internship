a835367b198138cd01904289c379090fa38c5692
Alokananda Y <plantsfortrees@gmail.com>
2024-06-16 10:38:22 +0530

    React upload icon

--- resume-builder/src/pages/CreateTemplate.jsx
+++ resume-builder/src/pages/CreateTemplate.jsx
@@ -13,21 +13,19 @@ const CreateTemplate = () => {
     uri: null,
     progress: 0,
   });
+
+  const [isUploadHovered, setIsUploadHovered] = useState(false);
+
   // handling the input field change
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevRec) => ({ ...prevRec, [name]: value }));
   };
+
   return (
-    <div
-      className="w-full px-4 lg:px-10 2xl:px-32
-  py-4 grid grid-cols-1 lg:grid-cols-12"
-    >
+    <div className="w-full px-4 lg:px-10 2xl:px-32 py-4 grid grid-cols-1 lg:grid-cols-12">
       {/* left container */}
-      <div
-        className="col-span-12 lg:col-span-4 2xl:col-span-3 w-full
-      flex flex-1 items-center justify-start flex-col gap-4 px-2"
-      >
+      <div className="col-span-12 lg:col-span-4 2xl:col-span-3 w-full flex flex-1 items-center justify-start flex-col gap-4 px-2">
         <div className="w-full">
           <p className="text-lg text-txtPrimary">Create a new Template</p>
         </div>
@@ -38,11 +36,9 @@ const CreateTemplate = () => {
           </p>
           <p className="text-sm text-txtDark capitalize font-bold">Template1</p>
         </div>
-        {/* template tile section */}
+        {/* template title section */}
         <input
-          className="w-full px-4 py-3 rounded-md bg-transparent
-        border-2 focus:border-orange-400 text-lg text-txtPrimary focus:text-txtDark
-        focus:shadow-md outline-none"
+          className="w-full px-4 py-3 rounded-md bg-transparent border-2 focus:border-orange-400 text-lg text-txtPrimary focus:text-txtDark focus:shadow-md outline-none"
           type="text"
           name="title"
           placeholder="Template Title"
@@ -50,39 +46,38 @@ const CreateTemplate = () => {
           onChange={handleInputChange}
         />
         {/* file uploader section */}
-        <div
-          className="w-full bg-orange-50 backdrop-blur-md h-[420px] lg:h-[620px] 2xl:h-[740px]
-      rounded-md border-2 border-dotted border-orange-300 cursor-pointer flex items-center justify-center"
-        >
+        <div className="w-full bg-orange-50 backdrop-blur-md h-[420px] lg:h-[620px] 2xl:h-[740px] rounded-md border-2 border-dotted border-orange-300 cursor-pointer flex items-center justify-center">
           {imageAsset.isImageLoading ? (
-            <React.Fragment>
-              <div className="flex flex-col items-center justify-center gap-4">
-                <PuffLoader color="orange" size={40} />
-                <p>{imageAsset?.progress.toFixed(2)}%</p>
-              </div>
-            </React.Fragment>
+            <div className="flex flex-col items-center justify-center gap-4">
+              <PuffLoader color="orange" size={40} />
+              <p>{imageAsset?.progress.toFixed(2)}%</p>
+            </div>
           ) : (
-            <React.Fragment>
-              {!imageAsset?.uri ? (
-                <React.Fragment>
-                  <label className="w-full cursor-pointer h-full">
-                    <div className="flex flex-col items-center justify-center h-full w-full">
-                      <div className="flex items-center justify-center cursor-pointer flex-col">
-                        <FaUpload className="text-3xl" />
-                      </div>
-                    </div>
-                  </label>
-                </React.Fragment>
-              ) : (
-                <React.Fragment></React.Fragment>
-              )}
-            </React.Fragment>
+            !imageAsset?.uri && (
+              <label className="w-full cursor-pointer h-full">
+                <div className="flex flex-col items-center justify-center h-full w-full">
+                  <div
+                    className="flex items-center justify-center cursor-pointer flex-col"
+                    onMouseEnter={() => setIsUploadHovered(true)}
+                    onMouseLeave={() => setIsUploadHovered(false)}
+                  >
+                    <FaUpload
+                      className={`text-5xl ${
+                        isUploadHovered ? "text-orange-600" : "text-gray-400"
+                      }`}
+                    />
+                  </div>
+                </div>
+              </label>
+            )
           )}
         </div>
       </div>
 
       {/* right container */}
-      <div className="col-span-12 lg:col-span-8 2xl:col-span-9">2</div>
+      <div className="col-span-12 lg:col-span-8 2xl:col-span-9">
+        Right Container Content
+      </div>
     </div>
   );
 };
