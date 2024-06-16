0dd56d226f6042f56d3fca10198362f7eab35220
Alokananda Y <plantsfortrees@gmail.com>
2024-06-16 09:26:56 +0530

    Creating a new template

--- resume-builder/src/pages/CreateTemplate.jsx
+++ resume-builder/src/pages/CreateTemplate.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const CreateTemplate = () => {
   return (
-	<div>CreateTemplate</div>
-  )
-}
+    <div
+      className="w-full px-4 lg:px-10 2xl:px-32
+  py-4 grid grid-cols-1 lg:grid-cols-12"
+    >
+      {/* left container */}
+      <div
+        className="col-span-12 lg:col-span-4 2xl:col-span-3 w-full
+      flex flex-1 items-center justify-start flex-col gap-4 px-2"
+      >
+        <div className="w-full">
+          <p>Create a new Template</p>
+        </div>
+      </div>
+      {/* right container */}
+      <div className="col-span-12 lg:col-span-8 2xl:col-span-9">2</div>
+    </div>
+  );
+};
 
-export default CreateTemplate
+export default CreateTemplate;
