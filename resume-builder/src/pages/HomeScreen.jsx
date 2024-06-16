c697512fcd0a81c220f83b45925ad4ccd00a9ec5
Alokananda Y <plantsfortrees@gmail.com>
2024-06-16 09:18:29 +0530

    Completed Routing

--- resume-builder/src/pages/HomeScreen.jsx
+++ resume-builder/src/pages/HomeScreen.jsx
@@ -1,7 +1,13 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Header, MainSpinner } from "../components";
-import HomeContainer from "../containers/HomeContainer";
+import { HomeContainer } from "../containers";
+import {
+  CreateTemplate,
+  UserProfile,
+  CreateResume,
+  TemplateDesignPinDetails,
+} from "../pages";
 
 const HomeScreen = () => {
   return (
@@ -13,6 +19,13 @@ const HomeScreen = () => {
         <Suspense fallback={<MainSpinner />}>
           <Routes>
             <Route path="/" element={<HomeContainer />} />
+            <Route path="/template/create" element={<CreateTemplate />} />
+            <Route path="/profile/:uid" element={<UserProfile />} />
+            <Route path="/resume/*" element={<CreateResume />} />
+            <Route
+              path="/resumeDetail/:templateID"
+              element={<TemplateDesignPinDetails />}
+            />
           </Routes>
         </Suspense>
       </main>
