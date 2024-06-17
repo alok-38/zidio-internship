befc36ffb2b07c8fdb9df3fcb1083c4fdb132925
Alokananda Y <plantsfortrees@gmail.com>
2024-06-17 17:38:32 +0530

    globalFilter

--- resume-builder/src/components/Filters.jsx
+++ resume-builder/src/components/Filters.jsx
@@ -3,15 +3,25 @@ import { MdLayersClear } from "react-icons/md";
 import { AnimatePresence, motion } from "framer-motion";
 import { slideUpDownWithScale } from "../animations";
 import { FiltersData } from "../utils/helpers";
+import useFilters from "../hooks/useFilters";
+import { useQuery, useQueryClient } from "react-query";
 
 const Filters = () => {
   const [isClearHovered, setIsClearHovered] = useState(false);
+  const { data: filterData, isLoading, isError } = useFilters();
+  const queryClient = useQueryClient();
+
+  const handleFilterValue = (value) => {
+    queryClient.setQueryData("globalFilter", {
+      ...queryClient.getQueryData("globalFilter"),
+      searchTerm: value,
+    });
+  };
+
   return (
     <div className="w-full flex items-center justify-start py-4">
       <div
-        className="border border-orange-300 rounded-md
-		px-3 py-2 mr-2 cursor-pointer group hover:shadow-md
-		bf-orange-200 relative"
+        className="border border-orange-300 rounded-md px-3 py-2 mr-2 cursor-pointer group hover:shadow-md bf-orange-200 relative"
         onMouseEnter={() => setIsClearHovered(true)}
         onMouseLeave={() => setIsClearHovered(false)}
       >
@@ -20,30 +30,27 @@ const Filters = () => {
           {isClearHovered && (
             <motion.div
               {...slideUpDownWithScale}
-              className="absolute -top-8 -left-2 bg-white shadow-md
-			rounded-md px-2 py-1"
+              className="absolute -top-8 -left-2 bg-white shadow-md rounded-md px-2 py-1"
             >
               <p className="whitespace-nowrap text-xs">Clear all</p>
             </motion.div>
           )}
         </AnimatePresence>
       </div>
-      <div
-        className="w-full flex items-center justify-start overflow-x-scroll
-	  gap-6 scrollbar-none"
-      >
+      <div className="w-full flex items-center justify-start overflow-x-scroll gap-6 scrollbar-none">
         {FiltersData &&
           FiltersData.map((item) => (
-            <div
-              key={item.id}
-              className={`border border-orange-300
-				rounded-md py-2 px-6 cursor-pointer group hover:shadow-md`}
+            <button
+              key={item.value}
+              onClick={() => handleFilterValue(item.value)}
+              className={`border border-orange-300 rounded-md px-6 py-2 cursor-pointer group hover:shadow-md ${
+                filterData && filterData.searchTerm === item.value ? "bg-orange-400 shadow-md text-white" : ""
+              }`}
+              onFocus={() => setIsClearHovered(true)}
+              onBlur={() => setIsClearHovered(false)}
             >
-              <div
-                className="text-sm text-txtPrimary group-hover:text-txtDark
-				whitespace-nowrap"
-              ></div>
-            </div>
+              <p className="text-sm text-txtPrimary group-hover:text-txtDark whitespace-nowrap">{item.label}</p>
+            </button>
           ))}
       </div>
     </div>
