f3d00f87f93a882cac50454edb70bb102109ec05
Alokananda Y <plantsfortrees@gmail.com>
2024-06-17 19:31:12 +0530

    REsume builder

--- 230506_resumebuilder/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as AuthButtonWithProvider } from "./AuthButtonWithProvider";
-export { default as MainSpinner } from "./MainSpinner";
-export { default as Filters } from "./Filters";
-export { default as TemplateDesignPin } from "./TemplateDesignPin";
-export { default as TemplateDesignPinDetails } from "./TemplateDesignPinDetails";
--- 230506_resumebuilder/src/designs/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Template1 } from "./Template1";
-export { default as Template2 } from "./Template2";
