39f60fbcf5e68a0a68efbdb8ce22711d403b230b
Alokananda Y <plantsfortrees@gmail.com>
2024-06-18 18:50:53 +0530

    navbar

--- job-portal/src/components/header/index.js
+++ job-portal/src/components/header/index.js
@@ -1,9 +1,14 @@
-import { AlignJustify } from "lucide-react";
+"use client";
+import { AlignJustify, Moon } from "lucide-react";
 import { Button } from "../ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+import { useTheme } from "next-themes";
 
 function Header() {
+  const { theme, setTheme } = useTheme();
+
   const menuItems = [
     {
       label: "Home",
@@ -33,22 +38,45 @@ function Header() {
             </Button>
           </SheetTrigger>
           <SheetContent side="left">
-            <div className="mr-6 hidden lg:flex">
-              <div className="grid gap-2 py-6">
-                {menuItems.map((menuItem, index) =>
-                  menuItem.show ? (
-                    <Link key={index} href={menuItem.path}>
-                      {/* Use a div or span inside the Link */}
-                      <div className="flex w-full items-center py-2 text-lg hover:font-semibold">
-                        {menuItem.label}
-                      </div>
-                    </Link>
-                  ) : null
-                )}
-              </div>
+            <Link className="mr-6 hidden lg:flex" href={"/"}>
+              <h3>JOBSCO</h3>
+            </Link>
+            <div className="grid gap-2 py-6">
+              {menuItems.map((menuItem) =>
+                menuItem.show ? (
+                  <Link
+                    key={menuItem.label}
+                    href={menuItem.path}
+                    className="flex w-full items-center py-2 text-lg font-semibold"
+                  >
+                    {menuItem.label}
+                  </Link>
+                ) : null
+              )}
+              <UserButton afterSignOutUrl="/" />
             </div>
           </SheetContent>
         </Sheet>
+        <nav className="ml-auto hidden lg:flex gap-6 items-center">
+          {menuItems.map((menuItem) =>
+            menuItem.show ? (
+              <Link
+                key={menuItem.label}
+                href={menuItem.path}
+                onClick={() => sessionStorage.removeItem("filterParams")}
+                className="group inline-flex h-9 w-max items-center rounded-md  px-4 py-2 text-sm font-medium"
+              >
+                {menuItem.label}
+              </Link>
+            ) : null
+          )}
+          <Moon
+            className="cursor-pointer"
+            fill={theme === "dark" ? "light" : "dark"}
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+          />
+          <UserButton afterSignOutUrl="/" />
+        </nav>
       </header>
     </div>
   );
