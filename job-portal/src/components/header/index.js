efa096a1383085722fbe16ccb937dc6de644dac6
Alokananda Y <plantsfortrees@gmail.com>
2024-06-18 18:56:59 +0530

    JOB PORTAL

--- job-portal/src/components/header/index.js
+++ job-portal/src/components/header/index.js
@@ -1,12 +1,13 @@
 "use client";
-import { AlignJustify, Moon } from "lucide-react";
+
+import Link from "next/link";
 import { Button } from "../ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
-import Link from "next/link";
+import { AlignJustify, Moon } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
 import { useTheme } from "next-themes";
 
-function Header() {
+function Header({ user, profileInfo }) {
   const { theme, setTheme } = useTheme();
 
   const menuItems = [
@@ -32,20 +33,20 @@ function Header() {
       <header className="flex h-16 w-full shrink-0 items-center">
         <Sheet>
           <SheetTrigger asChild>
-            <Button>
+            <Button className="lg:hidden">
               <AlignJustify className="h-6 w-6" />
               <span className="sr-only">Toggle Navigation Menu</span>
             </Button>
           </SheetTrigger>
           <SheetContent side="left">
-            <Link className="mr-6 hidden lg:flex" href={"/"}>
+            <Link href={"/"} className="mr-6 hidden lg:flex">
               <h3>JOBSCO</h3>
             </Link>
             <div className="grid gap-2 py-6">
-              {menuItems.map((menuItem) =>
+              {menuItems.map((menuItem, index) =>
                 menuItem.show ? (
                   <Link
-                    key={menuItem.label}
+                    key={index} // Add key prop here
                     href={menuItem.path}
                     className="flex w-full items-center py-2 text-lg font-semibold"
                   >
@@ -53,18 +54,26 @@ function Header() {
                   </Link>
                 ) : null
               )}
+              <Moon
+                className="cursor-pointer mb-4"
+                fill={theme === "dark" ? "light" : "dark"}
+                onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+              />
               <UserButton afterSignOutUrl="/" />
             </div>
           </SheetContent>
         </Sheet>
+        <Link href={"/"} className="hidden font-bold text-3xl lg:flex mr-6">
+          JOB PORTAL
+        </Link>
         <nav className="ml-auto hidden lg:flex gap-6 items-center">
-          {menuItems.map((menuItem) =>
+          {menuItems.map((menuItem, index) =>
             menuItem.show ? (
               <Link
-                key={menuItem.label}
+                key={index} // Add key prop here
                 href={menuItem.path}
                 onClick={() => sessionStorage.removeItem("filterParams")}
-                className="group inline-flex h-9 w-max items-center rounded-md  px-4 py-2 text-sm font-medium"
+                className="group inline-flex h-9 w-max items-center rounded-md px-4 py-2 text-sm font-medium"
               >
                 {menuItem.label}
               </Link>
