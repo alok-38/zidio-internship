8204b087d0c45b5e07f5d476b1e20864ed44f408
Alokananda Y <plantsfortrees@gmail.com>
2024-06-18 16:13:57 +0530

    Sidebar

--- job-portal/src/components/common-layout/index.js
+++ job-portal/src/components/common-layout/index.js
@@ -1,8 +1,9 @@
+import  Header  from "../header";
 function CommonLayout({ children }) {
   return (
     <div className="mx-auto max-w-7xl p-6 lg:px-8">
       {/* Header Component */}
-
+      <Header />
       {/* Header Component */}
 
       {/* Main Content */}
