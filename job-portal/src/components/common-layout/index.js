e4de78040a4517b32332e3ae3b391379565fe853
Alokananda Y <plantsfortrees@gmail.com>
2024-06-18 15:44:08 +0530

    Job portal

---
+++ job-portal/src/components/common-layout/index.js
@@ -0,0 +1,16 @@
+function CommonLayout({ children }) {
+  return (
+    <div className="mx-auto max-w-7xl p-6 lg:px-8">
+      {/* Header Component */}
+
+      {/* Header Component */}
+
+      {/* Main Content */}
+      <main>{children}</main>
+
+      {/* Main Content */}
+    </div>
+  );
+}
+
+export default CommonLayout;
