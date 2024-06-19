81bbdef972534fb08b047df20f286eca8835cfee
Alokananda Y <plantsfortrees@gmail.com>
2024-06-19 08:42:43 +0530

    Onboarding

---
+++ job-portal/src/components/on-board/index.js
@@ -0,0 +1,35 @@
+"use client";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { TabsContent } from "@radix-ui/react-tabs";
+import { useEffect, useState } from "react";
+import CommonForm from "../common-form";
+import {
+  candidateOnboardFormControls,
+  initialCandidateFormData,
+  initialRecruiterFormData,
+  recruiterOnboardFormControls,
+} from "@/utils";
+
+import { useUser } from "@clerk/clerk-react";
+
+function onBoard() {
+  return (
+    <div className="bg-white">
+      <Tabs value={currentTab} onValueChange={handleTabChange}>
+        <div className="w-full">
+          <div className="flex items-baseline justify-between border-b pb-6 pt-24">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+              Welcome to onboarding
+            </h1>
+            <TabsList>
+              <TabsTrigger value="candidate">Candidate</TabsTrigger>
+              <TabsTrigger value="recruiter">Recruiter</TabsTrigger>
+            </TabsList>
+          </div>
+        </div>
+      </Tabs>
+    </div>
+  );
+}
+
+export default onBoard;
